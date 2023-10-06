0b4b1d19e26c79c6a4c4037eb3a38dfc8bdcfbd6
Chetancodex <ckkumar419@gmail.com>
2023-10-06 19:03:03 +0530

    order issue

--- controller/ordercontroller.js
+++ controller/ordercontroller.js
@@ -16,23 +16,23 @@ exports.getOrders = async(req,res) => {
 
 // Add Order
 exports.addOrders = async(req,res) => {
-   try {
-    const userId = req.userData.id;
-    const productId =  req.body.productId;
-    const quantity = req.body.quantity;
-    if(userId && productId && quantity) {
-       const orderItem = await Order.create({
-            userId : userId,
-            productId : productId,
-            quantity : quantity
-        });
-        await orderItem.save()
-        return res.status(200).send({ message : "Your Order is Placed" })
-    } else {
-      return res.status(400).send({ message : "Order is not Placed some Error Occured" })
-    }
-   } catch (error) {
-    console.log(error);
-    res.status(500).send({ message: "Internal server issue" });
-  }
+  try {
+   const userId = req.userData.id;
+   const productId =  req.body.productId;
+   const quantity = req.body.quantity;
+   if(userId && productId && quantity) {
+      const orderItem = await Order.create({
+           userId : userId,
+           productId : productId,
+           quantity : quantity
+       });
+       await orderItem.save()
+       return res.status(200).send({ message : "Your Order is Placed" })
+   } else {
+     return res.status(400).send({ message : "Order is not Placed some Error Occured" })
+   }
+  } catch (error) {
+   console.log(error);
+   res.status(500).send({ message: "Internal server issue" });
+ }
 }
